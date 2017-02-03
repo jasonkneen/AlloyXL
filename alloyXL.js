c4c15e53a6a2b1a636aac5e815b414c3bc14f7dd
jasonkneen <jason@bouncingfish.com>
2017-02-03 12:37:55 +0000

    Fixes for path based controllers + empty controllers

--- alloyXL.js
+++ alloyXL.js
@@ -2,57 +2,67 @@ Alloy.createController = function(name, args) {
 
     // create instance to the controller and view
     var controller = new(require("alloy/controllers/" + name))(args);
-    var view = controller.getView();
 
-    // initialise Alloy.Controllers
-    Alloy.Controllers = Alloy.Controllers || {};
+    // check if we have a view and not just <Alloy/>
+    if (controller.__views.length > 0) {
 
-    // save the controller to Alloy.Controllers for global access
-    Alloy.Controllers[name] = controller;
+        var view = controller.getView();
 
-    // add a once event handler
-    controller.once = function(name, callback) {
-        controller.on(name, function() {
-            controller.off(name);
-            callback();
-        });
-        return controller;
-    };
+        // initialise Alloy.Controllers
+        Alloy.Controllers = Alloy.Controllers || {};
 
-    // if we're dealing with a Ti view that has open() method, add in
-    // event handlers for open, and close to clean up
-    if (typeof view.open === "function") {
+        // handles opening controllers in folders e.g. screens/login
+        var path = name.split("/");
 
-        // fire an open event on open
-        view.addEventListener("open", function(e) {
-            controller.trigger("open", e);
-            if (ENV_DEV) console.log("controller " + name + " was opened");
-        });
+        // if we have a path, set name to the last part (controller name)
+        if (path.length > 0) name = path[path.length - 1];
 
-        // fully clean up the view and controller on close
-        view.addEventListener("close", function(e) {
-            view = null;
-            controller.off();
-            controller.destroy();
-            Alloy.Controllers[controller.id] = null;
-            delete Alloy.Controllers[controller.id];
-            controller = null;
+        // save the controller to Alloy.Controllers for global access
+        Alloy.Controllers[name] = controller;
 
-            if (ENV_DEV) console.log("Controller " + name + " cleaned up!");
-        });
+        // add a once event handler
+        controller.once = function(eventName, callback) {
+            controller.on(eventName, function() {
+                controller.off(eventName);
+                callback();
+            });
+            return controller;
+        };
 
-		// fire an open event on open
-        view.addEventListener("postlayout", function(e) {
-            controller.trigger("postlayout", e);
-            if (ENV_DEV) console.log("controller " + name + " layout finished");
-        });
+        if (typeof view.open == "function") {
 
-    } else {
-        // fire an open event on open
-        view.addEventListener("postlayout", function(e) {
-            controller.trigger("postlayout", e);
-            if (ENV_DEV) console.log("controller " + name + " layout finished");
-        });
+            // fire an open event on open
+            view.addEventListener("open", function(e) {
+                controller.trigger("open", e);
+                if (ENV_DEV) console.log("controller " + name + " was opened");
+            });
+
+            // fully clean up the view and controller on close
+            view.addEventListener("close", function(e) {
+                view = null;
+                controller.off();
+                controller.destroy();
+                Alloy.Controllers[controller.id] = null;
+                delete Alloy.Controllers[controller.id];
+                controller = null;
+
+                if (ENV_DEV) console.log("Controller " + name + " cleaned up!");
+            });
+
+            // fire an open event on open
+            view.addEventListener("postlayout", function(e) {
+                controller.trigger("postlayout", e);
+                if (ENV_DEV) console.log("controller " + name + " layout finished");
+            });
+
+        } else {
+            // fire an open event on open
+            view.addEventListener("postlayout", function(e) {
+                controller.trigger("postlayout", e);
+                if (ENV_DEV) console.log("controller " + name + " layout finished");
+            });
+        }
+
     }
 
     return controller;
