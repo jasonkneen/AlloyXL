73b3e0f44509ed5241f3b1d656da18e84f7a04dc
jasonkneen <jason@bouncingfish.com>
2017-02-09 10:01:56 +0000

    check if view has .open method

--- alloyXL.js
+++ alloyXL.js
@@ -5,7 +5,7 @@ Alloy.createController = function(name, args) {
 
     // check if we have a view and not just <Alloy/>
 
-    if (Object.keys(controller.__views).length>0) {
+    if (Object.keys(controller.__views).length > 0) {
         var view = controller.getView();
 
         // initialise Alloy.Controllers
@@ -29,40 +29,45 @@ Alloy.createController = function(name, args) {
             return controller;
         };
 
-        if (typeof view.open == "function") {
+        if (typeof view.addEventListener == "function") {
+            if (typeof view.open == "function") {
 
-            // fire an open event on open
-            view.addEventListener("open", function(e) {
-                controller.trigger("open", e);
-                if (ENV_DEV) console.log("controller " + name + " was opened");
-            });
+                // fire an open event on open
+                view.addEventListener("open", open = function(e) {
+                    view.removeEventListener("open", open);
+                    controller.trigger("open", e);
+                    if (ENV_DEV) console.log("controller " + name + " was opened");
+                });
 
-            // fully clean up the view and controller on close
-            view.addEventListener("close", function(e) {
-                view = null;
-                controller.off();
-                controller.destroy();
-                Alloy.Controllers[controller.id] = null;
-                delete Alloy.Controllers[controller.id];
-                controller = null;
+                // fully clean up the view and controller on close
+                view.addEventListener("close", function close(e) {
+                    view.removeEventListener("close", close);
+                    view = null;
+                    Alloy.Controllers[controller.id] = null;
+                    delete Alloy.Controllers[controller.id];
+                    controller.off();
+                    controller.destroy();
+                    controller = null;
 
-                if (ENV_DEV) console.log("Controller " + name + " cleaned up!");
-            });
+                    if (ENV_DEV) console.log("Controller " + name + " cleaned up!");
+                });
 
-            // fire an open event on open
-            view.addEventListener("postlayout", function(e) {
-                controller.trigger("postlayout", e);
-                if (ENV_DEV) console.log("controller " + name + " layout finished");
-            });
+                // fire an open event on open
+                view.addEventListener("postlayout", function postlayout(e) {
+                    view.removeEventListener("postlayout", postlayout);
+                    controller.trigger("postlayout", e);
+                    if (ENV_DEV) console.log("controller " + name + " layout finished");
+                });
 
-        } else {
-            // fire an open event on open
-            view.addEventListener("postlayout", function(e) {
-                controller.trigger("postlayout", e);
-                if (ENV_DEV) console.log("controller " + name + " layout finished");
-            });
+            } else {
+                // fire an open event on open
+                view.addEventListener("postlayout", function postlayout(e) {
+                    view.removeEventListener("postlayout", postlayout);
+                    controller.trigger("postlayout", e);
+                    if (ENV_DEV) console.log("controller " + name + " layout finished");
+                });
+            }
         }
-
     }
 
     return controller;
