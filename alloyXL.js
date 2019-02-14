04fdd257d89d062153704f8aece3e09339a5dae0
Jason Kneen <jason.kneen@bouncingfish.com>
2019-02-14 13:57:52 +0000

    formatting

--- alloyXL.js
+++ alloyXL.js
@@ -1,63 +1,43 @@
 Alloy.createController = function (name, args) {
 
     function cleanUpController(controller) {
-
-        // remove our global pointer
         Alloy.Controllers[controller.id] = null;
 
-        // iterate sub-views and clean them out -- normally not 
-        // necessary but because AlloyXL is intercepting the 
-        // createController method and doing it's thing, we have 
-        // to clean up after ourselves.
-
         if (controller.__views) {
-            _.each(controller.__views, function (value, key) {
+            _.each(controller.__views, function (value) {
                 cleanUpController(value);
             });
         }
 
-        // turn off any event handlers and destroy any data-binding
         if (controller.__iamalloy) {
             controller.off();
             controller.destroy();
         }
 
-        // kill the pointer
         controller = null;
     }
 
-    // create instance to the controller and view
-    var controller = new(require("alloy/controllers/" + name))(args);
+    var controller = new (require("alloy/controllers/" + name))(args);
 
-    // check if we have a view and not just <Alloy/>
     if (Object.keys(controller.__views).length > 0) {
         var view = controller.getView();
 
-        // initialise Alloy.Controllers
         Alloy.Controllers = Alloy.Controllers || {};
 
-        // handles opening controllers in folders e.g. screens/login
         var path = name.split("/");
 
-        // if we have a path, set name to the last part (controller name)
         if (path.length > 0) name = path[path.length - 1];
 
-        // Avoid having issues with same controller name within different folders
         if (Alloy.Controllers[name] && !controller.getView().id) {
-            console.warn("::AlloyXL:: The controller Alloy.Controllers." + name + " (" + controller.__controllerPath + ") exists, and will be overwriten because it's conflicting with another controller already instanciated with the same name. " +
-                "Please add a unique ID on the top parent view within that controller view so you can use this as the controller name within AlloyXL to avoid this.");
+            console.warn("::AlloyXL:: The controller Alloy.Controllers." + name + " (" + controller.__controllerPath + ") exists, and will be overwriten because it's conflicting with another controller already instanciated with the same name. Please add a unique ID on the top parent view within that controller view so you can use this as the controller name within AlloyXL to avoid this.");
         }
 
-        // No matter what, the ID on the top parent view will always 
-        // override the controller name driven from its filename.
         if (controller.getView().id) {
             name = controller.getView().id;
         }
 
-        // save the controller to Alloy.Controllers for global access
         Alloy.Controllers[name] = controller;
 
-        // add a once event handler
         controller.once = function (eventName, callback) {
             controller.on(eventName, function () {
                 controller.off(eventName);
@@ -66,18 +46,15 @@ Alloy.createController = function (name, args) {
             return controller;
         };
 
-        if (typeof view.addEventListener == "function") {
-            if (typeof view.open == "function") {
-
-                // fire an open event on open
+        if (typeof view.addEventListener === "function") {
+            if (typeof view.open === "function") {
                 view.addEventListener("open", function open(e) {
                     view.removeEventListener("open", open);
                     controller.trigger("open", e);
-                    if (ENV_DEV) console.log("::AlloyXL:: controller " + name + " was opened");
+                    if (true) console.log("::AlloyXL:: controller " + name + " was opened");
                 });
 
-                // fully clean up the view and controller on close
-                view.addEventListener("close", function close(e) {
+                view.addEventListener("close", function close() {
                     view.removeEventListener("close", close);
                     view = null;
 
@@ -85,22 +62,19 @@ Alloy.createController = function (name, args) {
 
                     controller = null;
 
-                    if (ENV_DEV) console.log("::AlloyXL:: Controller " + name + " cleaned up!");
+                    if (true) console.log("::AlloyXL:: Controller " + name + " cleaned up!");
                 });
 
-                // fire an open event on open
                 view.addEventListener("postlayout", function postlayout(e) {
                     view.removeEventListener("postlayout", postlayout);
                     controller.trigger("postlayout", e);
-                    if (ENV_DEV) console.log("::AlloyXL:: controller " + name + " layout finished");
+                    if (true) console.log("::AlloyXL:: controller " + name + " layout finished");
                 });
-
             } else {
-                // fire an open event on open
                 view.addEventListener("postlayout", function postlayout(e) {
                     view.removeEventListener("postlayout", postlayout);
                     controller.trigger("postlayout", e);
-                    if (ENV_DEV) console.log("::AlloyXL:: controller " + name + " layout finished");
+                    if (true) console.log("::AlloyXL:: controller " + name + " layout finished");
                 });
             }
         }
